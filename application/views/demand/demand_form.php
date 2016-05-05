39f07bcfffedf204085ca8b95d42005c8bb872a1
xavanem <vanemelen.xavier@hotmail.com>
2016-05-05 14:28:22 +0200

    Modify form

--- application/views/demand/demand_form.php
+++ application/views/demand/demand_form.php
@@ -1,86 +1,59 @@
-<div id="header">
-  <?php echo ($info); ?>
-  <h1 style="text-align: center; text-decoration: underline;"><br />Choisissez votre type de transport</h1>
-
-  <script>
-
-  $(document).ready(function(){
-    $('#next').click(function(e){
-        console.log($('input[name=transport]:checked').val());
-        document.getElementById("formMove").style.display = "block";
-    });
-  });
+<style type="text/css">#D1, #D2, #D3, #D4 {display: none;}</style>
+<script type="text/javascript">
+function showRadio() {
+	var n = document.form.btnr.length;
+	for(i=1;i<=n;i++) {
+		if(document.getElementById('choix'+i).checked == true) {
+			document.getElementById('D'+i).style.display = "block";
+		} else {
+			document.getElementById('D'+i).style.display = "none";
+		}
+	}
+  }
 </script>
-<form name="typeOfTransport" method="post" >
-  <input type="radio" name="transport" value="move" /> <label for="transport">Déménagement</label>
-  <input type="radio" name="transport" value="vehicle" /> <label for="transport">Véhicule</label>
-  <input type="radio" name="transport" value="person" /> <label for="transport">Personnes</label>
-  <input type="radio" name="transport" value="objects" /> <label for="transport">Objets divers</label>
+<p><b>Faites votre choix: </b></p>
+<form name="form-inline" action="" method="post">
+	<p>
+		<label>Déménagement <input type="radio" id="choix1" name="btnr" value="dem" onclick="showRadio()" /></label>
+		<label>Véhicules <input type="radio" id="choix2" name="btnr" value="veh" onclick="showRadio()" /></label>
+		<label>Personnes <input type="radio" id="choix3" name="btnr" value="per" onclick="showRadio()"  /></label>
+		<label>Objets divers <input type="radio" id="choix4" name="btnr" value="obj" onclick="showRadio()" /></label>
+	</p>
+  <div id="D1">
+    <div class="form-group">
+      <label for="exampleInputName2">Name</label>
+      <input type="text" class="form-control" id="exampleInputName2" placeholder="Jane Doe">
+    </div>
+    <div class="form-group">
+      <label for="exampleInputEmail2">Email</label>
+      <input type="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
+    </div>
+    <button type="submit" class="btn btn-default">Send invitation</button>
+  </div>
+	<div id="D1">
+    <div class="form-group">
+      <label for="exampleInputName2">Name</label>
+      <input type="text" class="form-control" id="exampleInputName2" placeholder="Jane Doe">
+    </div>
+    <div class="form-group">
+      <label for="exampleInputEmail2">Email</label>
+      <input type="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
+    </div>
+    <button type="submit" class="btn btn-default">Send invitation</button>
+	</div>
+
+	<div id="D2">
+		<label>Prénom de l'amateur <input type="text" /></label> <label>Département de l'amateur <input type="text" /></label>
+	</div>
+
+	<div id="D3">
+		<label>Nom de l'animalerie <input type="text" /></label> <label>Département de l'animalerie <input type="text" /></label>
+	</div>
+
+	<div id="D4">
+		<label>Nom de l'association <input type="text" /></label> <label>Département de l'association <input type="text" /></label>
+	</div>
+
 </form>
-<button id="next"> Suivant </button>
-
-  <table id="formMove" class="tableInscription">
-    <form id="frmInscr" action="" method="post">
-      <tr>
-        <td>
-          <label for="email">Email:</label>
-        </td>
-        <td>
-          <input class="input" type="text" name="email" id="email" value="<?php if(isset($_POST["email"])) echo $_POST["email"]; ?>" />
-        </td>
-      </tr>
-
-      <tr>
-        <td>
-          <label for="prenom">Pr&eacute;nom:</label>
-        </td>
-        <td>
-          <input class="input" type="text" name="prenom" id="prenom" value="<?php if(isset($_POST["prenom"])) echo $_POST["prenom"]; ?>"/>
-        </td>
-      </tr>
-
-      <tr>
-        <td>
-          <label for="nom">Nom:</label>
-        </td>
-        <td>
-          <input class="input" type="text" name="nom" id="nom" value="<?php if(isset($_POST["nom"])) echo $_POST["nom"]; ?>"/>
-        </td>
-      </tr>
-
-      <tr>
-        <td>
-          <label for="mdp">Mot de passe:</label>
-        </td>
-        <td>
-          <input class="input" type="password" name="mdp" id="mdp" />
-        </td>
-      </tr>
-
-      <tr>
-        <td>
-          <label for="confirm">Confirmer:</label>
-        </td>
-        <td>
-          <input class="input" type="password" name="confirm" id="confirm" />
-        </td>
-      </tr>
-      <tr>
-        <td><br />  </td>
-      </tr>
-      <tr>
-        <td>
-          <br />
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <input style="border-radius: 4px;" class="input" type="submit" name="btInscr" id="btInscr" value="Inscription" />
-        </td>
-      </tr>
-    </form>
-  </table>
-</div>
-<div style="min-height: 140px;" id="content">
-
-</div>
+</body>
+</html>
