7a3d9faf307d2279d4f4c86385c68ad4bcf6c266
ntscharner <nico.tscharner@gmail.com>
2016-05-05 11:44:25 +0200

    ajout de la page register_trans
    register_trans sera le formulaire d'inscription des ransporteurs
    [en dév par nts]

---
+++ application/views/auth/register_trans.php
@@ -0,0 +1,13 @@
+<!--register_form pour les transporteurs
+work on progress
+nts
+-->
+
+<?php
+<?php echo form_open($this->uri->uri_string()); ?>
+<div class="page-header">
+	<h1>Enregistrement d'annonceur</h1>
+</div>
+
+
+?>
