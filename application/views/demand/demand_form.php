4c9b2449961d352c25259e3a5e7721534115128b
xavanem <vanemelen.xavier@hotmail.com>
2016-05-05 17:21:13 +0200

    Form Demand V0
    J'ai pas encore fini mais je vais rentrer... ;-)

--- application/views/demand/demand_form.php
+++ application/views/demand/demand_form.php
@@ -37,67 +37,117 @@ $(document).ready(function() {
 	})
 })
 </script>
+<div class="col-md-1"></div>
 <p><b>Faites votre choix: </b></p>
 <form name="form-inline" action="" method="post">
+  <div class="col-md-1"></div>
 	<div class="btn-group choice" role="group" aria-label="...">
 		<button type="button" class="btn btn-default" id="dem">Déménagement</button>
 		<button type="button" class="btn btn-default" id="veh">Véhicules</button>
 		<button type="button" class="btn btn-default" id="per">Personnes</button>
 		<button type="button" class="btn btn-default" id="obj">Objets divers</button>
 	</div>
+  <br /><br />
 	<div class="row">
-		<div class="col-md-6">
-		  <div class="form-group">
-					<?php echo form_label('Rue, Nr', $street['id']); ?><?php echo form_input($street); ?>
-					<span style="color: red;"><?php echo form_error($street['name']); ?><?php echo isset($errors[$street['name']])?$errors[$street['name']]:''; ?></span>
-				<br>
-					<?php echo form_label('NPA / Lieu ', $zip_code['id']); ?>
-					<div class="row">
-						<div class="col-md-4">
-							<?php echo form_input($zip_code); ?>
-						</div>
-						<div class="col-md-8">
-							<?php echo form_input($address); ?>
-						</div>
-					</div>
-					<span style="color: red;"><?php echo form_error($address['name']); ?><?php echo isset($errors[$address['name']])?$errors[$address['name']]:''; ?></span>
-					<span style="color: red;"><?php echo form_error($zip_code['name']); ?><?php echo isset($errors[$zip_code['name']])?$errors[$zip_code['name']]:''; ?></span>
-		  </div>
-		</div>
-		<div class="col-md-6">
-		  <div class="form-group">
-					<?php echo form_label('Rue, Nr', $street['id']); ?><?php echo form_input($street); ?>
-					<span style="color: red;"><?php echo form_error($street['name']); ?><?php echo isset($errors[$street['name']])?$errors[$street['name']]:''; ?></span>
-				<br>
-					<?php echo form_label('NPA / Lieu ', $zip_code['id']); ?>
-					<div class="row">
-						<div class="col-md-4">
-							<?php echo form_input($zip_code); ?>
-						</div>
-						<div class="col-md-8">
-							<?php echo form_input($address); ?>
-						</div>
-					</div>
-					<span style="color: red;"><?php echo form_error($address['name']); ?><?php echo isset($errors[$address['name']])?$errors[$address['name']]:''; ?></span>
-					<span style="color: red;"><?php echo form_error($zip_code['name']); ?><?php echo isset($errors[$zip_code['name']])?$errors[$zip_code['name']]:''; ?></span>
-		  </div>
-		</div>
+    <div class="col-md-1"></div>
+    <fieldset class="col-md-5">
+      <legend class="scheduler-border">Départ</legend>
+    		<div class="col-md-12">
+    		  <div class="form-group">
+    					<?php echo form_label('Rue, Nr', $street['id']); ?><?php echo form_input($street); ?>
+    					<span style="color: red;"><?php echo form_error($street['name']); ?><?php echo isset($errors[$street['name']])?$errors[$street['name']]:''; ?></span>
+    				<br>
+    					<?php echo form_label('NPA / Lieu ', $zip_code['id']); ?>
+    					<div class="row">
+    						<div class="col-md-4">
+    							<?php echo form_input($zip_code); ?>
+    						</div>
+    						<div class="col-md-8">
+    							<?php echo form_input($address); ?>
+    						</div>
+    					</div>
+    					<span style="color: red;"><?php echo form_error($address['name']); ?><?php echo isset($errors[$address['name']])?$errors[$address['name']]:''; ?></span>
+    					<span style="color: red;"><?php echo form_error($zip_code['name']); ?><?php echo isset($errors[$zip_code['name']])?$errors[$zip_code['name']]:''; ?></span>
+    		  </div>
+    		</div>
+    </fieldset>
+    <fieldset class="scheduler-border col-md-5">
+      <legend class="scheduler-border">Arrivée</legend>
+  		<div class="col-md-12">
+  		  <div class="form-group">
+  					<?php echo form_label('Rue, Nr', $street['id']); ?><?php echo form_input($street); ?>
+  					<span style="color: red;"><?php echo form_error($street['name']); ?><?php echo isset($errors[$street['name']])?$errors[$street['name']]:''; ?></span>
+  				<br>
+  					<?php echo form_label('NPA / Lieu ', $zip_code['id']); ?>
+  					<div class="row">
+  						<div class="col-md-4">
+  							<?php echo form_input($zip_code); ?>
+  						</div>
+  						<div class="col-md-8">
+  							<?php echo form_input($address); ?>
+  						</div>
+  					</div>
+  					<span style="color: red;"><?php echo form_error($address['name']); ?><?php echo isset($errors[$address['name']])?$errors[$address['name']]:''; ?></span>
+  					<span style="color: red;"><?php echo form_error($zip_code['name']); ?><?php echo isset($errors[$zip_code['name']])?$errors[$zip_code['name']]:''; ?></span>
+  		  </div>
+  		</div>
+    </fieldset>
+    <div class="col-md-1"></div>
 	</div>
 	<div class="div_dem div_demand">
-    <div class="form-group">
-      <label for="exampleInputName2">Name</label>
-      <input type="text" class="form-control" id="exampleInputName2" placeholder="Jane Doe">
+    <div class="form-group col-md-3">
+      <label for="nbreBoxes">Nombres de cartons</label>
+      <input type="number" class="form-control col-md-1" id="nbreBoxes" value="40">
     </div>
-    <div class="form-group">
-      <label for="exampleInputEmail2">Email</label>
-      <input type="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
+    <div class="form-group col-md-3">
+      <label for="nbreSmFournitures">Nombre petits meubles</label>
+      <input type="number" class="form-control" id="nbreSmFournitures" value="20">
     </div>
-    <button type="submit" class="btn btn-default">Send invitation</button>
+    <div class="form-group col-md-3">
+      <label for="nbreMdFournitures">Nombre meubles moyens</label>
+      <input type="number" class="form-control" id="nbreMdFournitures" value="10">
+    </div>
+    <div class="form-group col-md-3">
+      <label for="nbreBgFournitures">Nombre gros meubles</label>
+      <input type="number" class="form-control" id="nbreBgFournitures" value="5">
+    </div>
+    <br /><br />
+    <div class="col-md-5"></div>
+    <button type="submit" class="btn btn-default">Envoyer la demande</button>
 	</div>
 
 	<div class="div_veh div_demand">
-		vehic
-		<label>Prénom de l'amateur <input type="text" /></label> <label>Département de l'amateur <input type="text" /></label>
+    <div class="form-group col-md-3">
+      <label for="brand">Marque</label>
+      <input type="text" class="form-control" id="brand" placeholder="Mercedes">
+    </div>
+    <div class="form-group col-md-3">
+      <label for="model">Modèle</label>
+      <input type="text" class="form-control" id="model" placeholder="S600">
+    </div>
+    <div class="form-group col-md-3">
+      <label for="length">Longueur</label>
+      <input type="text" class="form-control" id="length">
+    </div>
+    <div class="form-group col-md-3">
+      <label for="width">Largeur</label>
+      <input type="text" class="form-control" id="width" >
+    </div>
+    <div class="form-group col-md-3">
+      <label for="height">Hauteur</label>
+      <input type="text" class="form-control" id="height" >
+    </div>
+    <div class="form-group col-md-3">
+      <label for="wheight">Poids</label>
+      <input type="text" class="form-control" id="wheight" placeholder="1300Kg">
+    </div>
+    <div class="form-group col-md-3">
+      <label for="quant">Quantité</label>
+      <input type="number" class="form-control" id="quant" placeholder="1">
+    </div>
+    <br /><br /><br /><br /><br /><br /><br /><br />
+    <div class="col-md-5"></div>
+    <button type="submit" class="btn btn-default">Envoyer la demande</button>
 	</div>
 
 	<div class="div_per div_demand">
