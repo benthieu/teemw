d49d70d101ae933dfe5833ac86f8c78c6f502697
benthieu <mathieu.b93@gmail.com>
2016-05-27 17:14:44 +0200

    reverse xav

--- application/controllers/Landing_page.php
+++ application/controllers/Landing_page.php
@@ -1,44 +1,24 @@
-
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><div class="page-header"><h1><?php echo lang('landing_page') ?></h1>
-</div>
 
+class Landing_page extends CI_Controller {
 
-
-<div class="container">
-  <br>
-  <div id="myCarousel" class="carousel slide carousel-global" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-      <div class="item active">
-        <img src="img/truck-landscape.jpg" alt="truck-landscape" width="460" height="345">
-      </div>
-
-      <div class="item">
-        <img src="img/crossing-roads.jpg" alt="crossing-roads" width="460" height="345">
-      </div>
-
-      <div class="item">
-        <img src="img/truck.jpg" alt="truck" width="460" height="345">
-      </div>
-    </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div>
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('landing_page');
+	}
+}
