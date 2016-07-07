87f3c0717671efa8ef941b52e38eacb85bf2c883
ntscharner <nico.tscharner@gmail.com>
2016-07-07 18:49:51 +0200

    home_page
    un jolie (mais moche) carousel sur home page

--- application/views/landing_page.php
+++ application/views/landing_page.php
@@ -5,46 +5,51 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><div class="page-header"><h2><?php echo lang('landing_page') ?></h2>
 
 </div>
-<h3><p class="text-info">TRANSPORT</p>
-<p class="text-success">ECOLOGIE</p>
-<p class="text-danger">ECONOMIE</p>
-<p class="text-warning">MARCHANDISE</p>
-<p class="text-primary">WARE</p>
-</h2>
-<div class="container">
 
-  <br>
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
+  <div id="carousel-example" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
+      <li data-target="#carousel-example" data-slide-to="1"></li>
+      <li data-target="#carousel-example" data-slide-to="2"></li>
+      <li data-target="#carousel-example" data-slide-to="3"></li>
     </ol>
 
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
+    <div class="carousel-inner">
       <div class="item active">
-        <img src="img/truck-landscape.jpg" alt="truck-landscape" width="460" height="345">
+        <a href="#"><img src="img/transport.jpg" /></a>
+        <div class="carousel-caption">
+        <h2>TRANSPORT</h2>
+        </div>
       </div>
-
       <div class="item">
-        <img src="img/crossing-roads.jpg" alt="crossing-roads" width="460" height="345">
+        <a href="#"><img src="img/ecologie.jpg" /></a>
+        <div class="carousel-caption">
+        <h2>ECOLOGIE</h2>
+        </div>
       </div>
-
       <div class="item">
-        <img src="img/hand-truck.jpg" alt="hand-truck" width="460" height="345">
+        <a href="#"><img src="img/economie.jpg" /></a>
+        <div class="carousel-caption">
+            <h2>ECONOMIE</h2>
+        </div>
+      </div>
+      <div class="item">
+        <a href="#"><img src="img/marchandises.jpg" /></a>
+        <div class="carousel-caption">
+            <h2>MARCHANDISE/WARE</h2>
+        </div>
       </div>
     </div>
 
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
+    <a class="left carousel-control" href="#carousel-example" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
     </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
+    <a class="right carousel-control" href="#carousel-example" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
     </a>
   </div>
-</div>
+  <style>
+.h2{
+  color: white;
+}
+  </style>
