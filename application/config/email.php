e7530657e114a5b7e5231d88a05554e07d1c056d
benthieu <mathieu.b93@gmail.com>
2016-07-07 18:21:11 +0200

    email config

--- application/config/email.php
+++ application/config/email.php
@@ -13,6 +13,24 @@
 $config['mailtype'] = 'html';
 $config['charset'] = 'utf-8';
 $config['newline'] = "\r\n";
+$config['useragent'] = 'CodeIgniter';
+$config['protocol'] = 'smtp';
+$config['mailpath'] = '/usr/sbin/sendmail -t -i';
+$config['smtp_host'] = 'ssl://smtp.googlemail.com';
+$config['smtp_user'] = 'enjamath@gmail.com';
+$config['smtp_pass'] = 'planedry';
+$config['smtp_port'] = 465;
+$config['smtp_timeout'] = 5;
+$config['wordwrap'] = TRUE;
+$config['wrapchars'] = 76;
+$config['mailtype'] = 'html';
+$config['charset'] = 'utf-8';
+$config['validate'] = FALSE;
+$config['priority'] = 3;
+$config['crlf'] = "\r\n";
+$config['newline'] = "\r\n";
+$config['bcc_batch_mode'] = FALSE;
+$config['bcc_batch_size'] = 200;
 
 
 /* End of file email.php */
